cee942450bbab30d22ae5ce5ca58e74c37227119
Arjun Pawar <108388620+Arjun0023@users.noreply.github.com>
2024-01-21 22:53:18 +0530

    Q4

---
+++ Q4/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import './App.css'
+
+const ParaGenrator = ()=>{
+  const [wordLength, setWordLenght] = useState(5);
+  const[genratedPara , setGenratedPara] = useState('');
+
+  const genratePara = ()=>{
+    const words = [
+      'Lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur',
+      'adipiscing', 'elit', 'sed', 'do', 'eiusmod', 'tempor',
+      'incididunt', 'ut', 'labore', 'et', 'dolore', 'magna', 
+      'aliqua'
+    ];
+
+    const paragraph = Array.from({length:wordLength},()=>{
+      const randomIndex = Math.floor(Math.random()*wordLength);
+      return words[randomIndex];
+    }).join('');
+    setGenratedPara(paragraph);
+  };
+
+  return (
+    <div>
+    <label>
+    Enter the Number of words:
+    <input type="number" value={wordLength}onChange={(e)=> setWordLenght(e.target.value)}></input>
+    </label>
+    <button onClick={genratePara}>Genrate Paragraph</button>
+    <p>{genratedPara}</p>
+    </div>
+  )
+  }
+
+export default ParaGenrator;
