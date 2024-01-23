89d59236d1b791bfc079c04e0a55affbe2c13f1d
Arjun Pawar <108388620+Arjun0023@users.noreply.github.com>
2024-01-23 23:29:26 +0530

    minor

--- Q4/src/App.jsx
+++ Q4/src/App.jsx
@@ -10,6 +10,9 @@ const ParaGenrator = ()=>{
       'Lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur',
       'adipiscing', 'elit', 'sed', 'do', 'eiusmod', 'tempor',
       'incididunt', 'ut', 'labore', 'et', 'dolore', 'magna', 
+      'aliqua','Lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur',
+      'adipiscing', 'elit', 'sed', 'do', 'eiusmod', 'tempor',
+      'incididunt', 'ut', 'labore', 'et', 'dolore', 'magna', 
       'aliqua'
     ];
 
